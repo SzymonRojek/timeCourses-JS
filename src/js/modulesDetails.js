b28102a14a46dc5d5410c16ba78a27373a37b14f
Szymon Rojek <sz.rojek@gmail.com>
2022-04-07 00:55:53 +0100

    create main obj with the future data

---
+++ src/js/modulesDetails.js
@@ -0,0 +1,38 @@
+export const modulesDetails = [
+  {
+    title: "Master JavaScript",
+    tutor: "good friends",
+    prefix: "all",
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    percent: 0,
+  },
+  {
+    title: "Fundamentals of JavaScript",
+    tutor: "Adam",
+    prefix: "[1",
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    percent: 0,
+  },
+  {
+    title: "Web Browser Environment",
+    tutor: "Przemek",
+    prefix: "[2",
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    percent: 0,
+  },
+  {
+    title: "In the world of Fameworks",
+    tutor: "Marcin",
+    prefix: "[3",
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    percent: 0,
+  },
+];
