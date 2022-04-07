533ff9e7f2f5c01652fe29aff9ccc28c02c6c01d
Szymon Rojek <sz.rojek@gmail.com>
2022-04-07 12:27:18 +0100

    create index file and export all files from the helpers dir

---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+export { default as countMinutesAndHours } from "./countMinutesAndHours";
+export { default as countSeconds } from "./countSeconds";
+export { default as getTotalDuration } from "./getTotalDuration";
