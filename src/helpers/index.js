624f7a63117a562d746bd0297a113cc8570e4594
Szymon Rojek <sz.rojek@gmail.com>
2022-04-07 13:41:25 +0100

    remove export

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,2 @@
 export { default as countMinutesAndHours } from "./countMinutesAndHours";
-export { default as countSeconds } from "./countSeconds";
-export { default as getTotalDuration } from "./getTotalDuration";
+export { default as getDuration } from "./getDuration";
